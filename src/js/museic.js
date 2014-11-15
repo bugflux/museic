7330187fe018ca4df2fa7e10f454b7a28613593c
André Prata <ndray@Andres-MacBook-Pro.local>
2014-11-15 12:28:07 +0000

    Handle some of the JSLint comments

--- src/js/museic.js
+++ src/js/museic.js
@@ -10,7 +10,7 @@ var Mosaic = function (opt) {
 		x: opt.x,
 		y: opt.y
 
-	}, map, r, c, rnd, coords, coord, divs, div, dim = {};
+	};
 
 	var prefetch = function (link) {
 		(new Image()).src = link;
@@ -55,106 +55,118 @@ var Mosaic = function (opt) {
 		return false;
 	};
 
-	// allocate the mapping grid and "remaining" coordinates
-	map = new Array(o.y);
-	coords = [];
-	for (r = 0; r < o.y; r++) {
-		map[r] = new Array(o.x);
+	var doTheMagic = function() {
+		var map, r, c, rnd, coords, coord, divs, div, dim = {};
 
-		for (c = 0; c < o.x; c++){
-			coords.push({
-				x: c,
-				y: r
-			});
-		}
-	}
+		// allocate the mapping grid and "remaining" coordinates
+		map = new Array(o.y);
+		coords = [];
+		for (r = 0; r < o.y; r++) {
+			map[r] = new Array(o.x);
 
-	shuffle(coords);
-	shuffle(o.imgs);
-
-	divs = document.createElement('div');
-
-	while(coords.length > 0) {
-		coord = coords.pop();
-
-		// check if coord is taken
-		if (map[coord.y][coord.x]) {
-			continue;
-		}
-
-		// free, create div
-		div = document.createElement('div');
-
-		// from this coordinate, try to "grow" the rectangle in one of four directions,
-		// with equal probability. growth stops once an obstacle is found.
-		dim.w = dim.h = 1;
-		dim.stop = false;
-
-		while (!dim.stop) {
-			rnd = Math.random();
-			if (rnd >= 0.75) {
-				// test w+1 right
-				if (coord.x + dim.w === o.x) {
-					dim.stop = true;
-				} else {
-					dim.w++;
-					if ((dim.stop = colCollides(map, { x: coord.x + dim.w - 1, y: coord.y }, dim))) {
-						dim.w--;
-					}
-				}
-			} else if (rnd >= 0.5) {
-				// test w+1 left
-				if (coord.x === 0) {
-					dim.stop = true;
-				} else {
-					dim.w++;
-					coord.x--;
-					if ((dim.stop = colCollides(map, coord, dim))) {
-						dim.w--;
-						coord.x++;
-					}
-				}
-			} else if (rnd >= 0.25) {
-				// test h+1 down
-				if (coord.y + dim.h === o.y) {
-					dim.stop = true;
-				} else {
-					dim.h++;
-					if ((dim.stop = rowCollides(map, { x: coord.x, y: coord.y + dim.h - 1 }, dim))) {
-						dim.h--;
-					}
-				}
-			} else {
-				// test h+1 up
-				if (coord.y === 0) {
-					dim.stop = true;
-				} else {
-					dim.h++;
-					coord.y--;
-					if ((dim.stop = rowCollides(map, coord, dim))) {
-						dim.h--;
-						coord.y++;
-					}
-				}
+			for (c = 0; c < o.x; c++){
+				coords.push({
+					x: c,
+					y: r
+				});
 			}
 		}
 
-		// mark the positions as occupied
-		tagMapPositions(map, coord, dim);
+		shuffle(coords);
+		shuffle(o.imgs);
 
-		// style div according to those limits
-		div.className = "museic-tile";
-		div.style.position = 'absolute';
-		div.style.top = ((coord.y * 100) / o.y + 0.5) + '%';
-		div.style.left = ((coord.x * 100) / o.x + 0.5) + '%';
-		div.style.height = ((dim.h * 100) / o.y - 1) + '%';
-		div.style.width = ((dim.w * 100) / o.x - 1) + '%';
+		divs = document.createElement('div');
 
-		div.style.backgroundImage = "url('" + prefetch(o.imgs.pop()) + "')";
+		while(coords.length > 0) {
+			coord = coords.pop();
 
-		// save the div
-		divs.appendChild(div);
-	}
+			// check if coord is taken
+			if (map[coord.y][coord.x]) {
+				continue;
+			}
 
-	o.elem.appendChild(divs);
+			// free, create div
+			div = document.createElement('div');
+
+			// from this coordinate, try to "grow" the rectangle in one of four directions,
+			// with equal probability. growth stops once an obstacle is found.
+			dim.w = dim.h = 1;
+
+			while (true) {
+				rnd = Math.random();
+				if (rnd >= 0.75) {
+					// test w+1 right
+					if (coord.x + dim.w === o.x) {
+						break;
+					}
+
+					dim.w++;
+					if (colCollides(map, { x: coord.x + dim.w - 1, y: coord.y }, dim)) {
+						dim.w--;
+						break;
+					}
+
+				} else if (rnd >= 0.5) {
+					// test w+1 left
+					if (coord.x === 0) {
+						break;
+					}
+
+					dim.w++;
+					coord.x--;
+					if (colCollides(map, coord, dim)) {
+						dim.w--;
+						coord.x++;
+						break;
+					}
+
+				} else if (rnd >= 0.25) {
+					// test h+1 down
+					if (coord.y + dim.h === o.y) {
+						break;
+					}
+
+					dim.h++;
+					if (rowCollides(map, { x: coord.x, y: coord.y + dim.h - 1 }, dim)) {
+						dim.h--;
+						break;
+					}
+
+				} else {
+					// test h+1 up
+					if (coord.y === 0) {
+						break;
+					}
+
+					dim.h++;
+					coord.y--;
+					if (rowCollides(map, coord, dim)) {
+						dim.h--;
+						coord.y++;
+						break;
+					}
+				}
+			}
+
+			// mark the positions as occupied
+			tagMapPositions(map, coord, dim);
+
+			// style div according to those limits
+			div.className = "museic-tile";
+			div.style.position = 'absolute';
+			div.style.top = ((coord.y * 100) / o.y + 0.5) + '%';
+			div.style.left = ((coord.x * 100) / o.x + 0.5) + '%';
+			div.style.height = ((dim.h * 100) / o.y - 1) + '%';
+			div.style.width = ((dim.w * 100) / o.x - 1) + '%';
+
+			div.style.backgroundImage = "url('" + prefetch(o.imgs.pop()) + "')";
+
+			// save the div
+			divs.appendChild(div);
+		}
+
+		o.elem.appendChild(divs);
+	};
+
+	doTheMagic();
 };
